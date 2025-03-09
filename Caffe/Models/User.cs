86c66a6ae07b97aab446e49d3ff933d577a347ae
gfi406 <129727313+gfi406@users.noreply.github.com>
2025-03-09 23:47:54 +0300

    fix 2

--- Caffe/Models/User.cs
+++ Caffe/Models/User.cs
@@ -10,7 +10,7 @@ namespace Caffe.Models
         public string email { get; set; }
         public string password { get; set; }
         public string phone { get; set; }
-        public string image { get; set; }
+        public string? image { get; set; }
         public Cart Cart { get; set; }
         public List<Order> Orders { get; set; }
 
